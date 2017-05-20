70c07e4e6c41e93869d74284409f9baf8e41dc9e
Austin <austingheen@gmail.com>
2017-05-19 20:13:06 -0700

    the game

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float speed;
+	public int ammo;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		ammo = 0;
+
+	}
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.CompareTag("Ammo"))
+		{
+			other.gameObject.SetActive(false);
+			ammo = ammo + 7;
+		}
+
+	}
+
+
+}
